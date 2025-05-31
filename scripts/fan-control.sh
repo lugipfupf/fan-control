d6cd4ac37d256313f35e58a9f51fc54aea0ab5c4
lugipfupf <lugi@lugipfupf.ch>
2025-05-31 23:12:45 +0200

    also set AIO pump speed

--- scripts/fan-control.sh
+++ scripts/fan-control.sh
@@ -8,14 +8,19 @@ temp=$(liquidctl --match kraken status | grep 'Liquid temperature' | awk '{print
 # Decide fan speed percentage (very basic curve)
 if (( $(echo "$temp < 28" | bc -l) )); then
   speed=0
+  pump=40
 elif (( $(echo "$temp < 30" | bc -l) )); then
   speed=40
+  pump=40
 elif (( $(echo "$temp < 35" | bc -l) )); then
   speed=60
+  pump=60
 elif (( $echo "$temp < 38" | bc -l )); then
   speed=80
+  pump=75
 else
   speed=100
+  pump=90
 fi
 
 # Set fan speed of all fan headers
@@ -24,6 +29,9 @@ for i in 1 2 3
 do
   liquidctl --match commander set fan$i speed $speed
 done
+
+# Set AIO pump speed
+liquidctl --match kraken set pump speed $pump
 
 # Uncomment for debugging only
 # echo "$(date): AIO water temp is ${temp}° --> Setting fan speeds to ${speed}%" >> /var/log/fan-control.log
